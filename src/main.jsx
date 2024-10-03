d062929102a6ed077a11804fa5300b339b33658b
bngure <brianwahinyangure@gmail.com>
2024-10-03 14:34:47 +0300

    Added timeout 2secs when initial loading react. Position of navbar configure to top-fixed

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+setTimeout(
+  () =>
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    ),
+  2000
 );
